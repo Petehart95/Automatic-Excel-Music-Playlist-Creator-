bf49e52d8f4dc9503e59ba039a6556506f91fe12
Peter Hart <peterhart95@hotmail.com>
2023-01-08 00:54:28 +0000

    Fixed skipping files issue
    File extension checker did not take into account the case sensitivity of the files, this was fixed.

---
+++ AutomaticExcelMusicPlaylistCollator/Project Files/getfilenames/FileProcessor.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using SpreadsheetLight;
+
+namespace AutomaticExcelMusicPlaylistCollator
+{
+    class FileProcessor 
+    {
+        public static int trackctr;
+        public static SLDocument doc;
+        public string path;
+        public int totalFiles;
+
+        static FileProcessor()
+        {
+            trackctr = 0;
+            doc = new SLDocument();
+        }
+
+        public void ProcessDirectory(string targetDirectory)
+        {
+            bool newArtist;
+            string lastFolderName = ""; string currentFolderName;
+
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+
+
+            foreach (string fileName in fileEntries)
+            {
+
+                currentFolderName = Path.GetFileName(Path.GetDirectoryName(fileName));
+                newArtist = false;
+                if (Path.GetExtension(fileName).ToUpper() == ".MP3" || Path.GetExtension(fileName).ToUpper() == ".WAV")
+                {
+                    trackctr = trackctr + 1;
+
+                    if (lastFolderName != currentFolderName)
+                    {
+                        lastFolderName = currentFolderName; newArtist = true;
+                    }
+                    ProcessFile(fileName, newArtist);
+
+                }
+            }
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+                ProcessDirectory(subdirectory);
+            }
+        }
+        public int GetDirectoryLength(string path)
+        {
+            return Directory.GetFiles(path, "*", SearchOption.AllDirectories).Length;
+        }
+
+        public void ProcessFile(string path, bool flag)
+        {
+            TagLib.File tagFile = TagLib.File.Create(path);
+            string title = tagFile.Tag.Title;
+            string lastFolderName = Path.GetFileName(Path.GetDirectoryName(path));
+            string artistCell; string trackCell;
+
+            if (flag == true)
+            {
+                artistCell = 'A' + trackctr.ToString();
+                doc.SetCellValue(artistCell, lastFolderName);
+
+                Console.WriteLine('\n' + lastFolderName);
+            }
+
+            trackCell = 'B' + trackctr.ToString();
+            doc.SetCellValue(trackCell, title);
+
+            Console.Write(trackctr + " - " + title);
+            Console.WriteLine();
+        }
+
+        public bool CreateNewFile(string pFileName, string pSheetName)
+        {
+            doc.RenameWorksheet("Sheet1", pSheetName);
+            doc.SaveAs(pFileName);
+
+            return true;
+        }
+
+        public bool AddNewSheet(string pFileName, string pSheetName)
+        {
+            if (!(doc.GetSheetNames(false).TrueForAll((sheetName) => sheetName.ToLower() == pSheetName.ToLower())))
+            {
+                doc.AddWorksheet(pSheetName);
+                doc.Save();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void SaveFile(string pFileName)
+        {
+
+            Cursor.Current = Cursors.WaitCursor;
+
+            doc.SaveAs(pFileName);
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            // Set cursor as default arrow
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
+
---
+++ AutomaticExcelMusicPlaylistCollator/Project Files/getfilenames/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using SpreadsheetLight;
+
+namespace AutomaticExcelMusicPlaylistCollator
+{
+    public class MusicCollator
+    {
+        [STAThread]
+
+        public static void Main()
+        {
+            string outDir, fileName;
+            FileProcessor fileProcessor = new FileProcessor();
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            Console.WriteLine("////////////////////////////////////////////////////");
+            Console.WriteLine("\tAutomatic Music Playlist Spreadsheet");
+            Console.WriteLine("////////////////////////////////////////////////////\n");
+
+            Console.WriteLine("Press any key to begin...");
+            Console.ReadKey();
+
+            Console.WriteLine("Please select the main music folder...");
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                fileProcessor.path = fbd.SelectedPath;
+                fileProcessor.totalFiles = fileProcessor.GetDirectoryLength(fileProcessor.path);
+                fileName = "myMusicPlaylist.xlsx";
+
+                outDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName;
+                outDir = outDir + "\\" + fileName;
+
+                Console.WriteLine("\nWill be Looking at files in this folder:");
+                Console.WriteLine(fileProcessor.path);
+                Console.WriteLine("Total Files Found Approximately (including non-music files): {0}", fileProcessor.totalFiles.ToString());
+
+                Console.WriteLine();
+                Console.WriteLine("This program will create an excel file based on all the music files in this folder.");
+                Console.WriteLine();
+
+                Console.WriteLine("Press any key to start...");
+                Console.ReadLine();
+
+                if (System.IO.Directory.Exists(fileProcessor.path))
+                {
+                    fileProcessor.ProcessDirectory(fileProcessor.path);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a valid file or directory.", fileProcessor.path);
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("All Files Found! Now creating an Excel file...");
+                Console.WriteLine();
+                fileProcessor.SaveFile(outDir);
+
+                Console.WriteLine("DONE! Spreadsheet has now been made! The spreadsheet file should be here: ");
+                Console.WriteLine();
+                Console.WriteLine(outDir);
+                Console.ReadLine();
+            }
+        }
+    }
+}
